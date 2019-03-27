8940bc83b7537e571e2d8ca5c99ca1edecaa637c
Wolfgang Popp <mail@wolfgang-popp.de>
2019-03-27 22:04:51 +0100

    Add theming support

--- README.md
+++ README.md
@@ -2,17 +2,14 @@
 
 <img src="screenshot.png" width="400" />
 
-A lightweight and fast Git prompt for Zsh heavily inspired by Olivier Verdier's
-[zsh-git-prompt](https://github.com/olivierverdier/zsh-git-prompt) and very similar to the "Informative VCS" prompt of
-fish shell. 
+A lightweight and fast Git prompt for Zsh heavily inspired by Olivier Verdier's [zsh-git-prompt](https://github.com/olivierverdier/zsh-git-prompt) and very similar to the "Informative VCS" prompt of fish shell.
 
-This prompt requires Git with `--porcelain=v2` support, which is available since version 2.11.0. You can check if your
-installation is compatible by executing `git status --branch --porcelain=v2` inside a Git repository.
+This prompt requires Git with `--porcelain=v2` support, which is available since version 2.11.0. You can check if your installation is compatible by executing `git status --branch --porcelain=v2` inside a Git repository.
 
 
 ## Installation
-Clone this repo or [download](https://raw.githubusercontent.com/woefe/zsh-git-prompt/master/git-prompt.zsh) the
-`git-prompt.zsh` file. Then add following lines to your `.zshrc`.
+Clone this repo or [download](https://raw.githubusercontent.com/woefe/zsh-git-prompt/master/git-prompt.zsh) the `git-prompt.zsh` file.
+Then add following lines to your `.zshrc`.
 
 ```
 prompt off  # Optional in some cases
@@ -42,9 +39,16 @@ The structure of the prompt is the following:
     * `…n`: there are `n` untracked files
 
 
+## Customization
+The appearance of the prompt can be adjusted by changing the variables that start with `ZSH_THEME_GIT_PROMPT_`.
+Take a look at the beginning of the [git-prompt.zsh](./git-prompt.zsh) file to see which variables are available and what their defaults are.
+Note that some of them are named differently than in the original Git prompt by Olivier Verdier.
+You can play around with different settings without reloading your shell.
+The Git prompt will automatically pick up your changes to the `ZSH_THEME_GIT_PROMPT_` variables.
+But remember to save them in your `.zshrc` after you tweaked them to your liking!
+
+
 ## Features / Non-Features
 * A pure shell implementation using awk; no Python, no Haskell required
 * Fast; git command is called only once
-* (Currently) no customization via environment variables. To customize the prompt you will have to edit the
-    `git-prompt.zsh` file.
 
--- git-prompt.zsh
+++ git-prompt.zsh
@@ -19,122 +19,147 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-
 setopt PROMPT_SUBST
+autoload -U colors
+colors
+
+
+: "${ZSH_THEME_GIT_PROMPT_PREFIX:="["}"
+: "${ZSH_THEME_GIT_PROMPT_SUFFIX:="] "}"
+: "${ZSH_THEME_GIT_PROMPT_SEPARATOR:="|"}"
+: "${ZSH_THEME_GIT_PROMPT_BRANCH:="%{$fg_bold[magenta]%}"}"
+: "${ZSH_THEME_GIT_PROMPT_BEHIND:="↓"}"
+: "${ZSH_THEME_GIT_PROMPT_AHEAD:="↑"}"
+: "${ZSH_THEME_GIT_PROMPT_UNMERGED:="%{$fg[red]%}✖"}"
+: "${ZSH_THEME_GIT_PROMPT_STAGED:="%{$fg[red]%}●"}"
+: "${ZSH_THEME_GIT_PROMPT_UNSTAGED:="%{$fg[blue]%}✚"}"
+: "${ZSH_THEME_GIT_PROMPT_UNTRACKED:="…"}"
+: "${ZSH_THEME_GIT_PROMPT_CLEAN:="%{$fg_bold[green]%}✔"}"
+
 
 function gitprompt() {
-    git status --branch --porcelain=v2 2>&1 | awk '
-        BEGIN {
-            RED = "%F{red}";
-            MAGENTA = "%F{magenta}";
-            GREEN = "%F{green}";
-            BLUE = "%F{blue}";
-            BOLD = "%B";
-            NB = "%b";
-            NC = "%f";
+    git status --branch --porcelain=v2 2>&1 | awk \
+        -v PREFIX="$ZSH_THEME_GIT_PROMPT_PREFIX" \
+        -v SUFFIX="$ZSH_THEME_GIT_PROMPT_SUFFIX" \
+        -v SEPARATOR="$ZSH_THEME_GIT_PROMPT_SEPARATOR" \
+        -v BRANCH="$ZSH_THEME_GIT_PROMPT_BRANCH" \
+        -v BEHIND="$ZSH_THEME_GIT_PROMPT_BEHIND" \
+        -v AHEAD="$ZSH_THEME_GIT_PROMPT_AHEAD" \
+        -v UNMERGED="$ZSH_THEME_GIT_PROMPT_UNMERGED" \
+        -v STAGED="$ZSH_THEME_GIT_PROMPT_STAGED" \
+        -v UNSTAGED="$ZSH_THEME_GIT_PROMPT_UNSTAGED" \
+        -v UNTRACKED="$ZSH_THEME_GIT_PROMPT_UNTRACKED" \
+        -v CLEAN="$ZSH_THEME_GIT_PROMPT_CLEAN" \
+        -v RC="%{$reset_color%}" \
+        '
+            BEGIN {
+                ORS = "";
 
-            fatal = 0;
-            oid = "";
-            head = "";
-            ahead = 0;
-            behind = 0;
-            untracked = 0;
-            unmerged = 0;
-            staged = 0;
-            unstaged = 0;
-        }
-
-        $1 == "fatal:" {
-            fatal = 1;
-        }
-
-        $2 == "branch.oid" {
-            oid = $3;
-        }
-
-        $2 == "branch.head" {
-            head = $3;
-        }
-
-        $2 == "branch.ab" {
-            ahead = $3;
-            behind = $4;
-        }
-
-        $1 == "?" {
-            ++untracked;
-        }
-
-        $1 == "u" {
-            ++unmerged;
-        }
-
-        $1 == "1" || $1 == "2" {
-            split($2, arr, "");
-            if (arr[1] != ".") {
-                ++staged;
-            }
-            if (arr[2] != ".") {
-                ++unstaged;
-            }
-        }
-
-        END {
-            if (fatal == 1) {
-                exit(1);
+                fatal = 0;
+                oid = "";
+                head = "";
+                ahead = 0;
+                behind = 0;
+                untracked = 0;
+                unmerged = 0;
+                staged = 0;
+                unstaged = 0;
             }
 
-            printf "[";
-
-            printf "%s", MAGENTA
-            printf "%s", BOLD
-            if (head == "(detached)") {
-                printf ":%s", substr(oid, 0, 7);
-            } else {
-                printf "%s", head;
-            }
-            printf "%s", NB
-            printf "%s", NC
-
-            if (behind < 0) {
-                printf "↓%d", behind * -1;
+            $1 == "fatal:" {
+                fatal = 1;
             }
 
-            if (ahead > 0) {
-                printf "↑%d", ahead;
+            $2 == "branch.oid" {
+                oid = $3;
             }
 
-            printf "|";
-
-            if (unmerged > 0) {
-                printf "%s", RED
-                printf "✖%d", unmerged;
-                printf "%s", NC
+            $2 == "branch.head" {
+                head = $3;
             }
 
-            if (staged > 0) {
-                printf "%s", RED
-                printf "●%d", staged;
-                printf "%s", NC
+            $2 == "branch.ab" {
+                ahead = $3;
+                behind = $4;
             }
 
-            if (unstaged > 0) {
-                printf "%s", BLUE
-                printf "✚%d", unstaged;
-                printf "%s", NC
+            $1 == "?" {
+                ++untracked;
             }
 
-            if (untracked > 0) {
-                printf "…%d", untracked;
+            $1 == "u" {
+                ++unmerged;
             }
 
-            if (unmerged == 0 && staged == 0 && unstaged == 0 && untracked == 0) {
-                printf "%s", GREEN
-                printf "✔"
-                printf "%s", NC
+            $1 == "1" || $1 == "2" {
+                split($2, arr, "");
+                if (arr[1] != ".") {
+                    ++staged;
+                }
+                if (arr[2] != ".") {
+                    ++unstaged;
+                }
             }
 
-            printf "] ";
-        }
-    '
+            END {
+                if (fatal == 1) {
+                    exit(1);
+                }
+
+                print PREFIX;
+
+                print BRANCH;
+                if (head == "(detached)") {
+                    print substr(oid, 0, 7);
+                } else {
+                    print head;
+                }
+                print RC;
+
+                if (behind < 0) {
+                    print BEHIND;
+                    printf "%d", behind * -1;
+                    print RC;
+                }
+
+                if (ahead > 0) {
+                    print AHEAD
+                    printf "%d", ahead;
+                    print RC;
+                }
+
+                print SEPARATOR;
+
+                if (unmerged > 0) {
+                    print UNMERGED;
+                    print unmerged;
+                    print RC;
+                }
+
+                if (staged > 0) {
+                    print STAGED;
+                    print staged;
+                    print RC;
+                }
+
+                if (unstaged > 0) {
+                    print UNSTAGED;
+                    print unstaged;
+                    print RC;
+                }
+
+                if (untracked > 0) {
+                    print UNTRACKED;
+                    print untracked;
+                    print RC;
+                }
+
+                if (unmerged == 0 && staged == 0 && unstaged == 0 && untracked == 0) {
+                    print CLEAN;
+                    print RC;
+                }
+
+                print SUFFIX;
+            }
+        '
 }
