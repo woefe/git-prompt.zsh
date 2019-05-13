3e18191af1754a111fea16bc6ae81afc557d7f5a
Wolfgang Popp <mail@wolfgang-popp.de>
2019-05-14 00:43:34 +0200

    Fix theming vars being overridden
    Only set theming variables if they are unset. I.e. do not override them
    if they are set to empty string.

--- git-prompt.zsh
+++ git-prompt.zsh
@@ -21,19 +21,19 @@
 
 autoload -U colors && colors
 
-: "${ZSH_THEME_GIT_PROMPT_PREFIX:="["}"
-: "${ZSH_THEME_GIT_PROMPT_SUFFIX:="] "}"
-: "${ZSH_THEME_GIT_PROMPT_SEPARATOR:="|"}"
-: "${ZSH_THEME_GIT_PROMPT_DETACHED:="%{$fg_bold[cyan]%}:"}"
-: "${ZSH_THEME_GIT_PROMPT_BRANCH:="%{$fg_bold[magenta]%}"}"
-: "${ZSH_THEME_GIT_PROMPT_BEHIND:="↓"}"
-: "${ZSH_THEME_GIT_PROMPT_AHEAD:="↑"}"
-: "${ZSH_THEME_GIT_PROMPT_UNMERGED:="%{$fg[red]%}✖"}"
-: "${ZSH_THEME_GIT_PROMPT_STAGED:="%{$fg[green]%}●"}"
-: "${ZSH_THEME_GIT_PROMPT_UNSTAGED:="%{$fg[red]%}✚"}"
-: "${ZSH_THEME_GIT_PROMPT_UNTRACKED:="…"}"
-: "${ZSH_THEME_GIT_PROMPT_STASHED:="%{$fg[blue]%}⚑"}"
-: "${ZSH_THEME_GIT_PROMPT_CLEAN:="%{$fg_bold[green]%}✔"}"
+: "${ZSH_THEME_GIT_PROMPT_PREFIX="["}"
+: "${ZSH_THEME_GIT_PROMPT_SUFFIX="] "}"
+: "${ZSH_THEME_GIT_PROMPT_SEPARATOR="|"}"
+: "${ZSH_THEME_GIT_PROMPT_DETACHED="%{$fg_bold[cyan]%}:"}"
+: "${ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"}"
+: "${ZSH_THEME_GIT_PROMPT_BEHIND="↓"}"
+: "${ZSH_THEME_GIT_PROMPT_AHEAD="↑"}"
+: "${ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[red]%}✖"}"
+: "${ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}●"}"
+: "${ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg[red]%}✚"}"
+: "${ZSH_THEME_GIT_PROMPT_UNTRACKED="…"}"
+: "${ZSH_THEME_GIT_PROMPT_STASHED="%{$fg[blue]%}⚑"}"
+: "${ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔"}"
 
 # Disable promptinit if it is loaded
 (( $+functions[promptinit] )) && {promptinit; prompt off}
